32c3666b662d5944b3ee5a50398854e019c3c380
José Redrejo <jose@learningequality.org>
2021-10-03 18:11:19 +0200

    Creación de los modelos

--- proyecto/scada/models.py
+++ proyecto/scada/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+tipos_datos = (
+    ("I", "Integer"),
+    ("F", "Float"),
+    ("b", "Bit"),
+    ("B", "Byte"),
+    ("W", "Word"),
+    ("DW", "Double_Word"),
+)
+
+
+class Tags(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    direccion = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=2, choices=tipos_datos)
+
+    def __str__(self):
+        return "{}-{}".format(self.nombre, self.direccion)
+
+
+class Valores(models.Model):
+    valor = models.FloatField(null=True)
+    fecha = models.DateTimeField(default=timezone.now)
+    tag = models.ForeignKey(Tags, related_name="tags", on_delete=models.CASCADE)
