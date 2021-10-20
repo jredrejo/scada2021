58d408bbebc6de34e99a03df5416d5c9a3ae163b
José Redrejo <jose@learningequality.org>
2021-10-20 09:47:10 +0200

    Creación de las plantillas

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('scada/', include('scada.urls'))
+    path('scada/', include('scada.urls')),
+    path('', include('scada.urls'))
 
 ]
--- proyecto/scada/views.py
+++ proyecto/scada/views.py
@@ -1,8 +1,19 @@
+import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Tags
+from .models import Valores
 
 
 def index(request):
-    return HttpResponse("Hola Mundo, primera app")
+    tags = Tags.objects.order_by("tipo").all()
+    datos = {"tags": tags, "ejemplo": datetime.datetime.now()}
+    return render(request, "scada/index.html", datos)
 
+
+def prueba(request):
+    tag = Tags.objects.get(id=1)
+    valores = Valores.objects.filter(tag=tag)
+    datos = {"uno": 111, "dos": 2222, "valores": valores}
+    return render(request, "scada/kk.html", datos)
